020eb0f69423984081401760a0dcb7a8c5e36559
Rizwan <rizwanrensa@gmail.com>
2019-06-25 23:52:33 -0400

    JSON dump upon clicking buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,70 @@
-let topics = ['Tom Cruise'];
+let actors = ['Tom Cruise', 'Arnold Schwarzenegger', 'Tom Hanks'];
 
+
+
+renderButtons();
+
+
+function renderButtons() {
+  for (var i = 0; i < actors.length; i++) {
+    var a = $('<button>');
+    a.addClass("actor");
+    a.attr("data-name", actors[i]);
+    a.text(actors[i]);
+    $("#buttons-view").append(a);
+  }
+}
+
+
+// Function for dumping the JSON content for each button into the div
+function displayMovieInfo() {
+
+  var actor = $(this).attr("data-name");
+  var queryURL = `https://api.giphy.com/v1/gifs/search?q=${
+    actor
+   }&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    $("#movies-view").text(JSON.stringify(response));
+  });
+}
+// var queryURL = `https://api.giphy.com/v1/gifs/search?q=${
+//     topics[0]
+//   }&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
 // Here we construct our URL
 //var queryURL = 'https://api.giphy.com?q=dog&apikey=trilogy';
+// topics.forEach(topic => {
+//   queryURL = `https://api.giphy.com/v1/gifs/search?q=${
+//     topics[topic]
+//   }&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
+// });
 
-var queryURL =
-  'https://api.giphy.com/v1/gifs/search?q=dog&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9';
+// $.ajax({
+//   url: queryURL,
+//   method: 'GET'
+// }).then(function (response) {
+//   console.log(response);
+//   var dogs = response.data;
 
-$.ajax({
-  url: queryURL,
-  method: 'GET'
-}).then(function(response) {
-  console.log(response);
+//   dogs.forEach(dog => {
+//     var imgURL = dog.images.fixed_height.url;
+//     console.log(imgURL);
 
-  var imgURL = response.data[0].images.fixed_height.url;
-  console.log(imgURL);
+//     //create a new div
+//     var giphyDiv = $("<div class='giphy'>");
 
-  document.getElementById('my_image').src = imgURL;
+//     //create an image tag and define the src
+//     var image = $('<img>').attr('src', imgURL);
 
-  //$('#movie-view').text(JSON.stringify(response));
-});
+//     //append the image to the div
+//     giphyDiv.append(image);
+
+//     //append the div to the image in the div
+//     $('#some_image').prepend(giphyDiv);
+//   });
+// });
+
+$(document).on("click", ".actor", displayMovieInfo);
