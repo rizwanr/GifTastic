f9fd025075d0aa1d9f55f67d640be88e0a4d15f5
Rizwan <rizwanrensa@gmail.com>
2019-06-26 00:00:48 -0400

    showing movie image on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,43 +28,24 @@ function displayMovieInfo() {
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    $("#movies-view").text(JSON.stringify(response));
-  });
+
+    var actors = response.data
+    actors.forEach(actor => {
+      var imgURL = actor.images.fixed_height.url;
+
+      //create a new div
+      var giphyDiv = $("<div class='giphy'>");
+
+      //create an image tag and define the src
+      var image = $('<img>').attr('src', imgURL);
+
+      //append the image to the div
+      giphyDiv.append(image);
+
+      //append the div to the image in the div
+      $('#movies-view').prepend(giphyDiv);
+    });
+  })
 }
-// var queryURL = `https://api.giphy.com/v1/gifs/search?q=${
-//     topics[0]
-//   }&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
-// Here we construct our URL
-//var queryURL = 'https://api.giphy.com?q=dog&apikey=trilogy';
-// topics.forEach(topic => {
-//   queryURL = `https://api.giphy.com/v1/gifs/search?q=${
-//     topics[topic]
-//   }&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
-// });
-
-// $.ajax({
-//   url: queryURL,
-//   method: 'GET'
-// }).then(function (response) {
-//   console.log(response);
-//   var dogs = response.data;
-
-//   dogs.forEach(dog => {
-//     var imgURL = dog.images.fixed_height.url;
-//     console.log(imgURL);
-
-//     //create a new div
-//     var giphyDiv = $("<div class='giphy'>");
-
-//     //create an image tag and define the src
-//     var image = $('<img>').attr('src', imgURL);
-
-//     //append the image to the div
-//     giphyDiv.append(image);
-
-//     //append the div to the image in the div
-//     $('#some_image').prepend(giphyDiv);
-//   });
-// });
 
 $(document).on("click", ".actor", displayMovieInfo);
