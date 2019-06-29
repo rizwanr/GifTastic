1d36163acbffad4ca678f6753539fb950eb73e53
Rizwan <rizwanrensa@gmail.com>
2019-06-28 23:09:47 -0400

    refactor ajax call and implement 10 extra giphy call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,96 @@
-let topics = ['Tom Cruise', 'Arnold Schwarzenegger', 'Tom Hanks', "Chris Hemsworth", "Chris Pratt", "Dwayne Johnson", "Will Smith", "Vin Diesel"];
-
+let topics = [
+  'Tom Cruise',
+  'Arnold Schwarzenegger',
+  'Tom Hanks',
+  'Chris Hemsworth',
+  'Chris Pratt',
+  'Dwayne Johnson',
+  'Will Smith',
+  'Vin Diesel',
+  'Pamela Anderson',
+  'Shahrukh khan',
+  'Hrithik Roshan'
+];
 
 //initial call of the function to render the buttons on screen
 renderButtons();
 
-
-
 function renderButtons() {
-  $("#buttons-view").empty()
+  $('#buttons-view').empty();
   for (var i = 0; i < topics.length; i++) {
     var a = $('<button>');
-    a.addClass("actor btn btn-info");
-    a.attr("data-name", topics[i]);
+    a.addClass('actor btn btn-info');
+    a.attr('data-name', topics[i]);
     a.text(topics[i]);
-    $("#buttons-view").append(a);
+    $('#buttons-view').append(a);
   }
 }
 
-
-// Function for dumping the JSON content for each button into the div
-function displayActorInfo() {
-
-  var actor = $(this).attr("data-name");
-  var queryURL = `https://api.giphy.com/v1/gifs/search?q=${
-    actor
-   }&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
-
+function ajaxCall(queryURL) {
   $.ajax({
     url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    $('#actors-view').empty();
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
 
-    console.log(response)
-
-    var actors = response.data
+    var actors = response.data;
     //for each of the JSON data
     actors.forEach(actor => {
       //grab the images.fixed_height.url
-      var imgURL = actor.images.downsized.url;
+      imgURL = actor.images.downsized.url;
       //grab the rating
-      var rating = actor.rating;
+      rating = actor.rating;
+
       // Creating an element to have the rating displayed
-      var pOne = $("<p>").text("Rating: " + rating);
+      pOne = $('<p>').text('Rating: ' + rating);
       //create a new div
-      var giphyDiv = $("<div class='giphy'>");
+      giphyDiv = $("<div class='giphy'>");
       //append the p element to the giphyDiv
       giphyDiv.append(pOne);
       //create an image tag and define the src
-      var image = $('<img>').attr('src', imgURL);
+      image = $('<img>').attr('src', imgURL);
       //append the image to the div
-      giphyDiv.append(image);
+      giphyDiv.prepend(image);
+
       //append the div to the image in the div
       $('#actors-view').prepend(giphyDiv);
     });
-  })
+  });
+}
+
+// Function for dumping the JSON content for each button into the div
+function displayActorInfo() {
+  var actor = $(this).attr('data-name');
+  if ($('#actors-view').is(':empty')) {
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${actor}&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
+
+    ajaxCall(queryURL);
+  } else {
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${actor}&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10`;
+
+    ajaxCall(queryURL);
+  }
 }
 
 // This function handles events where one button is clicked
-$("#add-actor").on("click", function (event) {
+$('#add-actor').on('click', function(event) {
   event.preventDefault();
   // This line grabs the input from the textbox
-  var actor = $("#actor-input").val().trim();
+  var actor = $('#actor-input')
+    .val()
+    .trim();
   //If there are text in thhe textbox then push it to the topics array
   if (actor) {
     topics.push(actor);
     console.log(topics);
   }
   //empty the value in the text-box
-  $('#actor-input').val(" ");
+  $('#actor-input').val(' ');
 
   //call the function to re-render the buttons in the screen after the update
   renderButtons();
-
 });
 
 // Function for displaying the actors info
 // Using $(document).on instead of $(".actor").on to add event listeners to dynamically generated elements
-$(document).on("click", ".actor", displayActorInfo);
+$(document).on('click', '.actor', displayActorInfo);
